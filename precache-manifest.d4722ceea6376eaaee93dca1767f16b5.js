5ed3a04042dc592e4296516b940deeedb0c54b6e
jdmman <gregorzamza2@gmail.com>
2018-12-11 16:22:53 +0300

    Updates

---
+++ precache-manifest.d4722ceea6376eaaee93dca1767f16b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "6ae8721d03b5128c2f37",
+    "url": "/my-app/static/js/main.6ae8721d.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/my-app/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "6ae8721d03b5128c2f37",
+    "url": "/my-app/static/css/main.2fee0174.chunk.css"
+  },
+  {
+    "revision": "a62bed72bd90f7893bf2a95ddc5f904d",
+    "url": "/my-app/index.html"
+  }
+];
